557b63dca4ba5502211eb69698b37ad42568a96f
陈随易 <chenbimo@gmail.com>
2025-08-01 23:39:07 +0800

    1

--- packages/befly/apis/tool/tokenCheck.js
+++ packages/befly/apis/tool/tokenCheck.js
@@ -7,24 +7,16 @@ export default Api.POST('令牌检测', false, {}, [], async (befly, ctx) => {
     try {
         const token = ctx.headers?.authorization?.split(' ')[1] || '';
         if (!token) {
-            return RNo('令牌不能为空', { state: 'no' });
+            return RNo('令牌不能为空');
         }
         const jwtData = await Jwt.verify(token);
-        return RYes(
-            '令牌有效',
-            {
-                state: 'yes'
-            },
-            {
-                detail: jwtData
-            }
-        );
+        return RYes('令牌有效');
     } catch (error) {
         befly.logger.error({
             msg: '令牌检测失败',
             error: error.message,
             stack: error.stack
         });
-        return RNo('令牌检测失败', { state: 'no' });
+        return RNo('令牌检测失败');
     }
 });
